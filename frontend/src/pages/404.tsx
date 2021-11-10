10f5d1c0040647d035499150d6b23891c95e30ad
Jacob Cambell <jacob@jacobcambell.com>
2021-11-09 17:24:01 -0700

    Remove default Gatsby contents

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <p>404 not found</p>
+  )
+}
+
+export default NotFoundPage
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <div>
+      Link Shortener
+    </div>
+  )
+}
+
+export default IndexPage
