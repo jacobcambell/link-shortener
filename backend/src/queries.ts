0a1c9e253dfbb5abecadb1c838b6a1dfda340d12
Jacob Cambell <jacob@jacobcambell.com>
2021-11-12 13:20:27 -0700

    Set up pg and dotenv

---
+++ backend/src/queries.ts
@@ -0,0 +1,12 @@
+require('dotenv').config()
+import { Client } from 'pg'
+
+export const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: Number(process.env.PGPORT)
+});
+
+client.connect();
