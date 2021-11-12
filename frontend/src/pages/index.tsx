89465f653f9d27ece255e78c1242eec5accabd39
Jacob Cambell <jacob@jacobcambell.com>
2021-11-12 12:19:06 -0700

    Create initial jumbo layout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,24 @@
 import * as React from "react"
+import '../styles/index.scss'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
-      Link Shortener
+      <div className='navbar'>
+        <Link to='/' className='logo'>xxd.pw</Link>
+      </div>
+
+      <div className="jumbo">
+        <p className="title">Easily Create Short Links</p>
+        {/* <p className="desc">Sign up for a free account to name, save, and view analytics on all your links.</p>
+        <a href="" className="signup">Sign Up Free</a> */}
+
+        <div className="row">
+          <input type="text" className="link" placeholder="Paste your link" />
+          <button className="btn">Shorten</button>
+        </div>
+      </div>
     </div>
   )
 }
