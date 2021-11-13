07485c2c4b03585c5f1ff4ce211a994535d2d1b3
Jacob Cambell <jacob@jacobcambell.com>
2021-11-12 18:46:54 -0700

    Create shortlink route and redirect user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers } from './schema/resolvers'
 import { typeDefs } from './schema/type-defs'
+import { client } from './queries';
+
 require('dotenv').config()
 
 const server = new ApolloServer({ typeDefs, resolvers })
@@ -12,6 +14,24 @@ server.listen({ port: process.env.GRAPHQL_SERVER_PORT }).then(({ port }) => {
 // Express
 import Express from 'express'
 const app = Express()
+
+app.get('/:shortlink', (req: Express.Request, res: Express.Response) => {
+    const shortlink = req.params.shortlink;
+
+    // Get the destination from this shortlink
+    client.query('SELECT destination FROM links WHERE shortlink=$1', [shortlink]).then((results) => {
+        if (results.rows.length === 0) {
+            // No links found with the provided shortcode
+            res.sendStatus(400)
+            return;
+        }
+
+        const destination = results.rows[0].destination;
+
+        res.redirect(destination)
+        return;
+    })
+})
 
 app.listen(process.env.EXPRESS_SERVER_PORT, () => {
     console.log('Express server listening on port ' + process.env.EXPRESS_SERVER_PORT)
