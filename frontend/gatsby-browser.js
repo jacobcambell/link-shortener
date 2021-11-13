c195e5f691891a20e8abc2ca01361ab32786e282
Jacob Cambell <jacob@jacobcambell.com>
2021-11-13 15:37:08 -0700

    Create wrapRootElement for Gatsby

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,1 +1,11 @@
-import './src/styles/global.scss'
+import React from 'react'
+import { ApolloWrapper } from './src/components/ApolloWrapper';
+import './src/styles/global.scss';
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <ApolloWrapper>
+            {element}
+        </ApolloWrapper>
+    )
+}
