9d973b8efb7f472e00c60da7d32af2334e50ef4b
Jacob Cambell <jacob@jacobcambell.com>
2021-11-12 18:37:03 -0700

    Setup express, use dotenv for ports

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,18 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers } from './schema/resolvers'
 import { typeDefs } from './schema/type-defs'
+require('dotenv').config()
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen().then(({ url }) => {
-    console.log('Server listening at ' + url)
+server.listen({ port: process.env.GRAPHQL_SERVER_PORT }).then(({ port }) => {
+    console.log('GraphQL server listening on port ' + port)
+})
+
+// Express
+import Express from 'express'
+const app = Express()
+
+app.listen(process.env.EXPRESS_SERVER_PORT, () => {
+    console.log('Express server listening on port ' + process.env.EXPRESS_SERVER_PORT)
 })
