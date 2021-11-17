00e84388d900191469cb773f74a1f8d328f29da8
Jacob Cambell <jacob@jacobcambell.com>
2021-11-17 13:06:22 -0700

    Use gatsby-plugin-apollo to fix broken build

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "link-shortener",
   },
-  plugins: ['gatsby-plugin-sass'],
+  plugins: ['gatsby-plugin-sass', {
+    resolve: 'gatsby-plugin-apollo',
+    options: {
+      uri: 'http://localhost:4000'
+    }
+  }],
 };
