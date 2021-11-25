55de21c0a23487cba5c94c717d6848d71a8c714f
Jacob Cambell <jacob@jacobcambell.com>
2021-11-24 20:04:51 -0700

    Import LinkList into Dashboard

---
+++ frontend/src/components/Analytics/LinkList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LinkList() {
+    return (
+        <div>
+            link list
+        </div>
+    )
+}
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Analytics from '../components/Analytics/GlobalAnalytics'
 import Navbar from '../components/Navbar/Navbar'
+import LinkList from '../components/Analytics/LinkList'
 
 export default function Dashboard() {
     return (
         <div>
             <Navbar></Navbar>
             <Analytics></Analytics>
+            <LinkList></LinkList>
         </div>
     )
 }
